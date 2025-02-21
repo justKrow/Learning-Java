092fa5639a1eec30c25090ca52b20a87e1a9f2e3
justKrow <krowxen2214@gmail.com>
2025-02-21 06:37:44 +0000

    data types

---
+++ src/datatype.java
@@ -0,0 +1,15 @@
+public class datatype {
+    public static void main(String[] args) {
+        int studentAge = 12;
+        double studentGPA = 3.5;
+        char studentFirstInitial = 'k';
+        char studentLastInitial = 'w';
+        boolean studentIsEnrolled = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(studentIsEnrolled);
+    }
+}
